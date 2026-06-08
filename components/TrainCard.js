d7b0390a588129c6f3ffdcba492b1e502eed8a78
류희송 <heesong@lyuhuisong-ui-MacBookAir.local>
2026-06-09 01:19:36 +0900

    Revert "Feat: 결제 완료 페이지 영수증 UI 개선"
    This reverts commit cea5f15a9a33db3abb80b4781b7cf05ff1f3c8b6.

--- components/TrainCard.js
+++ components/TrainCard.js
@@ -37,7 +37,7 @@
     }
   }
 
-  function seatSection(seat, first, trainNumber, seatIdx) {
+  function seatSection(seat, first) {
     var borderRight = first ? 'border-right:1px solid #e0eaf8;' : '';
     if (seat.dash) {
       return '<div class="seat-section" style="' + borderRight + '">' +
@@ -49,9 +49,7 @@
                '<span class="seat-label' + (seat.labelClass ? ' ' + seat.labelClass : '') + '">' + seat.label + '</span>' +
                '<span class="seat-count ' + (seat.countClass || '') + '">' + (seat.count || '') + '</span>' +
              '</div>' +
-             '<button class="price" data-price-btn data-train-number="' + trainNumber + '" data-seat-idx="' + seatIdx + '">' +
-               (seat.price || '') +
-             '</button>' +
+             '<div class="price">' + (seat.price || '') + '</div>' +
            '</div>';
   }
 
@@ -102,8 +100,8 @@
 
         /* 푸터 */
         '<div class="card-footer">',
-          seatSection(data.seats[0], true,  data.trainNumber, 0),
-          seatSection(data.seats[1], false, data.trainNumber, 1),
+          seatSection(data.seats[0], true),
+          seatSection(data.seats[1], false),
         '</div>',
 
       '</div>',
